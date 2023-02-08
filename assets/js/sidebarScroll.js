912abfc2f25504bc8209e6c8b2bfa793ecd5530a
Daniel Mogildea <danielmogildea@outlook.com>
2023-02-08 11:18:40 +0000

    Fixed scroll responsiveness on manual mode.

--- assets/js/sidebarScroll.js
+++ assets/js/sidebarScroll.js
@@ -1,16 +1,24 @@
-var selectedMode = "auto";
+var selectedMode = "auto"; // default selection; changes according to which strobe sync mode button is pressed
 
+// execute function on page load and window resize
 window.onresize = window.onload = function () {
-    if (selectedMode = "auto" && playerHidden == false && window.innerHeight <= 1050) {
-        document.getElementById("AM-bottom-position-container").classList.remove("absolute");
-    } else if (selectedMode = "auto" && playerHidden == true && window.innerHeight <= 940) {
-        document.getElementById("AM-bottom-position-container").classList.remove("absolute");
-    } else if (selectedMode = "manual" && playerHidden == false && window.innerHeight <= 685) {
-        document.getElementById("MM-bottom-position-container").classList.remove("absolute");
-    } else if (selectedMode = "manual" && playerHidden == true && window.innerHeight <= 565) {
-        document.getElementById("MM-bottom-position-container").classList.remove("absolute");
-    } else {
-        document.getElementById("AM-bottom-position-container").classList.add("absolute");
-        document.getElementById("MM-bottom-position-container").classList.add("absolute");
+    if (selectedMode = "auto") { // when auto strobe sync mode is selected
+        if (playerHidden == false && window.innerHeight <= 1050) { // if internal player is being shown and viewport height is lower or equals 1050px
+            document.getElementById("AM-bottom-position-container").classList.remove("absolute");
+        } else if (playerHidden == true && window.innerHeight <= 940) { // if internal player is hidden and viewport height is lower or equals 940px
+            document.getElementById("AM-bottom-position-container").classList.remove("absolute");
+        } else {
+            document.getElementById("AM-bottom-position-container").classList.add("absolute"); // in order to ignore DOM and stick element to the bottom
+        }
+    }
+
+    if (selectedMode = "manual") { // when manual strobe sync mode is selected
+        if (playerHidden == false && window.innerHeight <= 685) { // when internal player is being shown and viewport height is lower or equals685px
+            document.getElementById("MM-bottom-position-container").classList.remove("absolute");
+        } else if (playerHidden == true && window.innerHeight <= 565) { // when internal player is hidden and viewport height is lower or equals 565px
+            document.getElementById("MM-bottom-position-container").classList.remove("absolute");
+        } else {
+            document.getElementById("MM-bottom-position-container").classList.add("absolute"); // in order to ignore DOM and stick element to the bottom
+        }
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/**/*.{js}",
+    "./assets/**/*.js",
     "./index.html",
     "./node_modules/flowbite/**/*.js"
   ],
