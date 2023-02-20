220c403aca401e7a554ebee886c3eed9058aeeb9
Daniel Mogildea <danielmogildea@outlook.com>
2023-02-20 15:23:42 +0000

    Webpack implementation.

--- assets/js/sensSlider.js
+++
@@ -1,8 +0,0 @@
-var sensSlider = document.getElementById("sens-slider");
-var sensOutput = document.getElementById("sens-label");
-sensOutput.innerHTML = "DETECTION SENSITIVITY (" + sensSlider.value + "%)"; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-sensSlider.oninput = function() {
-    sensOutput.innerHTML = "DETECTION SENSITIVITY (" + this.value + "%)";
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./assets/**/*.js",
-    "./index.html",
-    "./node_modules/flowbite/**/*.js"
+    "src/assets/**/*.js",
+    "src/template.html",
+    "node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
